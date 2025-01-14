22e9515d07219681e16857b6d07d171d2189bf28
SUROX07 <suroxdas4u@gmail.com>
2025-01-14 11:55:24 +0530

    second commit

--- Backend Server/main.py
+++ Backend Server/main.py
@@ -3,12 +3,12 @@ import subprocess
 app =Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('testProg.html')
 
 @app.route('/start_game', methods=['POST'])
 def start_game():
     # Start the Python game here
-    subprocess.Popen(['python', 'camera.py'])
+    subprocess.Popen(['python', './camera.py'])
     return 'Starting Detection program....'
 
 if __name__ == '__main__':
